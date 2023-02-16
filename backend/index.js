f12aaef5e3e47de51c97431ccf7029752a32f0da
sebas-baltor <sebastian_1321124218@uptecamac.edu.mx>
2023-02-16 17:39:59 -0600

    test of socket.io working and ready to test, and new packages added like redux

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,20 @@
 import express from "express";
-import {Server} from "socket.io";
-import {createServer} from "http";
-
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
 const app = express();
 const server = createServer(app);
-// const server = server.createServer(app);
-const io = new Server(server);
-app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","*");
-    next();
-})
-server.listen(3000,()=>{
-    console.log("server in port 3000")
-})
+const io = new Server(server,{cors:"http://localhost:5173/"});
+app.use(cors());
 
-io.on("connection",(socket)=>{
-    console.log("new client connected")
-    socket.emit("connection",null);
-})
-
+io.on("connection", (socket) => {
+  console.log(`⚡ cliente connectado con id: ${socket.id}`);
+  io.emit("connection", "connectado");
+  socket.on("count", (arg) => {
+    console.log(`el contador es de ${arg}`);
+    io.emit("count", arg);
+  });
+});
+server.listen(3000, () => {
+  console.log("server in port 3000");
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,51 @@
-import { useEffect } from "react"
-import socketClient from "socket.io-client"
-import './App.css'
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+import "./App.css";
 
-const socket = socketClient("http://127.0.0.1:3000");
+const socket = io("http://localhost:3000");
 
 function App() {
-  
-  useEffect(()=>{
-    socket.on("connection",()=>{
-      console.log("I'm connected")
-    })
-    // console.log("se debio ejecutar el socket")
-  },[])
+  const [count, setCount] = useState(0);
+  const [connect, setConnect] = useState("no conectado");
+  useEffect(() => {
+    socket.on("connection", (socket) => {
+      setConnect("conectado");
+      console.log(socket.id)
+    });
+    socket.on("count", () => {
+      console.log("cambio la cuenta");
+    });
+    return ()=>{
+      socket.off("connection");
+      socket.off("count");
+    }
+  }, []);
   return (
     <div>
-      chat
+      chat {count}
+      <br />
+      {connect}
+      <br />
+      <button
+        className="bg-sky-100 rounded-full p-4"
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        aumentar contador
+      </button>
+      <button
+        className="bg-orange-200 rounded-full p-4"
+        onClick={(e) => {
+          e.preventDefault();
+          socket.emit("count", count);
+          // socket.off("count");
+        }}
+      >
+        Enviar valor contador
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
