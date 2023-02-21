fc26a68881062bb76e0d7dbb4ae3f2bbbafd2184
sebas-baltor <sebastian_1321124218@uptecamac.edu.mx>
2023-02-20 20:16:21 -0600

    styling the login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,28 +14,29 @@ function Login() {
       <div className="flex flex-col gap-12 p-4 w-[400px] max-w-[500px] bg-slate-50 rounded-lg">
         <h2 className="text-center text-4xl font-black">Iniciar Sesion</h2>
         <form onSubmit={formik.handleSubmit} className=" flex flex-col gap-3">
-          <label htmlFor="i__email" className="font-bold text-slate-700">
-            Username
+          <label htmlFor="i__email">
+            <span className="font-bold text-slate-700">Gmail</span>
+            <input
+              id="i__email"
+              type="email"
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              className="w-full p-2 outline-none border-0 rounded border-2 border-violet-200 hover:bg-violet-50 focus:border-violet-400"
+              placeholder="tucorreo@gmail.com"
+            />
           </label>
-          <input
-            id="i__email"
-            type="email"
-            name="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-            className="w-full p-2 outline-none border-0 rounded border-2 border-violet-200 hover:bg-violet-50 focus:border-violet-400"
-          />
-          <label htmlFor="i__password" className="font-bold text-slate-700">
-            Password
+          <label htmlFor="i__password">
+            <span className="font-bold text-slate-700">Password</span>
+            <input
+              id="i__password"
+              type="password"
+              name="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              className="w-full p-2 outline-none border-0 rounded border-2 border-violet-200 hover:bg-violet-50 focus:border-violet-400"
+            />
           </label>
-          <input
-            id="i__password"
-            type="password"
-            name="password"
-            onChange={formik.handleChange}
-            value={formik.values.password}
-            className="w-full p-2 outline-none border-0 rounded border-2 border-violet-200 hover:bg-violet-50 focus:border-violet-400"
-          />
           <button
             type="submit"
             className="px-3 py-2 rounded bg-violet-400 text-white w-1/2 lg:w-1/3 mx-auto"
